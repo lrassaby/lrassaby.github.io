e9a48d8707033aeb7e4373e5bbc1677c2f714631
Louis Rassaby <louis.rassaby@gmail.com>
2013-03-02 17:47:41 -0500

    debugging finding closest marker

--- maps/maps.js
+++ maps/maps.js
@@ -30,7 +30,7 @@ function initialize() {
 	mapRedLine();
 	getJSONlisting();
 	findMyLocation();
-	mypos = markers[0].position;
+	mypos = markers[1].position;
 	if(mypos != null) findClosestMarker(mypos);
 	else printMessage(document.createTextNode("Error: unable to find closest station."))
 }
