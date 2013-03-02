1ffc0d47272ab558586369fe9f59bd7e686ae538
Louis Rassaby <louis.rassaby@gmail.com>
2013-03-01 22:33:10 -0500

    added where in the world project for testing on web server

---
+++ maps/maps.js
@@ -0,0 +1,37 @@
+var map;
+var lat = 42.37501;
+var lng = -71.11619;
+
+function initialize() {
+	mapOptions = {
+		zoom: 11, 
+		center: new google.maps.LatLng(lat, lng),
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("canvas"), mapOptions);
+	findMyLocation();
+}
+		
+function findMyLocation() {
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			infowindow = new google.maps.InfoWindow({
+				map: map,
+				position: pos,
+				content: "My Location"
+			});
+		}, function() {
+			message = document.createTextNode("Error: cannot get geolocation.")
+		});
+	} else {  // Browser doesn't support Geolocation
+		message = document.createTextNode("Error: your browser's too old for this.")
+		printError(message);
+	}
+}
+
+function printError(message) {
+	topbar = document.getElementById("topbar");
+	topbar.appendChild(document.createElement("p"));
+	topbar.appendChild(message)
+}
