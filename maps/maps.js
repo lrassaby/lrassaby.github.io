78995173e4d8a374adf5e8b05b9522a0b2f36eed
Louis Rassaby <louis.rassaby@gmail.com>
2013-03-02 17:02:36 -0500

    trying different height

--- maps/maps.js
+++ maps/maps.js
@@ -1,5 +1,5 @@
 var map;
-var mypos;
+var mypos = null;
 var json_response = [];
 var lat = 42.30321;
 var lng = -71.09047;
@@ -30,6 +30,7 @@ function initialize() {
 	mapRedLine();
 	getJSONlisting();
 	findMyLocation();
+	findClosestMarker();
 }
 
 function makeRedLine() {
@@ -150,4 +151,30 @@ function printMessage(message) {
 	errorbar = document.getElementById("error");
 	errorbar.appendChild(document.createElement("p"));
 	errorbar.appendChild(message)
+}
+
+function rad(x) {return x*Math.PI/180;}
+
+function findClosestMarker() {
+	if(mypos == null) return;
+    var lat = mypos.latLng.lat();
+    var lng = mypos.latLng.lng();
+    var R = 6371; // radius of earth in km
+    var distances = [];
+    var closest = -1;
+    for(var i=0; i < map.markers.length; i++) {
+        var mlat = map.markers[i].position.lat();
+        var mlng = map.markers[i].position.lng();
+        var dLat  = rad(mlat - lat);
+        var dLong = rad(mlng - lng);
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(rad(lat)) * Math.cos(rad(lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        distances[i] = d;
+        if ( closest == -1 || d < distances[closest] ) {
+            closest = i;
+        }
+    }
+    alert(map.markers[closest].title);
 }
