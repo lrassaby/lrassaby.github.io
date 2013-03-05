29f63901512d2dc93f36ffc276f5c346b1918980
Louis Rassaby <louis.rassaby@gmail.com>
2013-03-04 21:58:50 -0500

    added a ton of stuff

--- maps/maps.js
+++ maps/maps.js
@@ -2,25 +2,18 @@ var map;
 var mypos;
 var lat = 42.30321;
 var lng = -71.09047;
+var open_infowindow = null;
 
 var redline_north = [];
-var north_names = ["Alewife Station", "Davis Station", "Porter Square Station", "Harvard Square Station", "Central Square Station", "Kendall/MIT Station", "Charles/MGH Station", "Park St. Station", "Downtown Crossing Station", "South Station", "Broadway Station", "Andrew Station", "JFK/UMass Station"];
-
 var redline_braintree = [];
-var braintree_names = ["JFK/UMass Station", "North Quincy Station", "Wollaston Station", "Quincy Center Station", "Quincy Adams Station", "Braintree Station"];
-
 var redline_ashmont = [];
-var ashmont_names = ["JFK/UMass Station", "Savin Hill Station", "Fields Corner Station", "Shawmut Station", "Ashmont Station"];
 
-
-var codes = []; 
 var t_icon = "assets/tsymbol.png";
 var carmen = "assets/carmen.png";
 var waldo = "assets/waldo.png";
-var markers = [];
+markers = [];
 var red = "#FF0000";
 
-var codes = [];
 
 function initialize() {
 	mapOptions = {
@@ -35,101 +28,113 @@ function initialize() {
 }
 
 function makeRedLine() {
+	var i = 0;
 	redline_north.push(new google.maps.LatLng(42.395428,-71.142483));   	// alewife
-	codes["Alewife Station"] = ["RALEN", null];
+	markers.push(new google.maps.Marker({title: "Alewife Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RALEN", scode: null}));
 	redline_north.push(new google.maps.LatLng(42.39674,-71.121815));    	// davis
-	codes["Davis Station"] = ["RDAVN", "RDAVS"];
+	markers.push(new google.maps.Marker({title: "Davis Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RDAVN", scode: "RDAVS"}));
 	redline_north.push(new google.maps.LatLng(42.3884,-71.119149));			// porter square
-	codes["Porter Square Station"] = ["RPORN", "RPORS"];
+	markers.push(new google.maps.Marker({title: "Porter Square Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RPORN", scode: "RPORS"}));
 	redline_north.push(new google.maps.LatLng(42.373362,-71.118956));		// harvard square
-	codes["Harvard Square Station"] = ["RHARN", "RHARS"];
+	markers.push(new google.maps.Marker({title: "Harvard Square Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RHARN", scode: "RHARS"}));
 	redline_north.push(new google.maps.LatLng(42.365486,-71.103802));   	// central square
-	codes["Central Square Station"] = ["RCENN", "RCENS"];
+	markers.push(new google.maps.Marker({title: "Central Square Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RCENN", scode: "RCENS"}));
 	redline_north.push(new google.maps.LatLng(42.36249079,-71.08617653));	// kendall/mit
-	codes["Kendall/MIT Station"] = ["RKENN", "RKENS"];
+	markers.push(new google.maps.Marker({title: "Kendall/MIT Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RKENN", scode: "RKENS"}));
 	redline_north.push(new google.maps.LatLng(42.361166,-71.070628));     	// charles/mgh
-	codes["Charles/MGH Station"] = ["RMGHN", "RMGHS"];
+	markers.push(new google.maps.Marker({title: "Charles/MGH Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RMGHN", scode: "RMGHS"}));
 	redline_north.push(new google.maps.LatLng(42.35639457,-71.0624242));	// park street
-	codes["Park St. Station"] = ["RPRKN", "RPRKS"];
+	markers.push(new google.maps.Marker({title: "Park St. Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RPRKN", scode: "RPRKS"}));
 	redline_north.push(new google.maps.LatLng(42.355518,-71.060225));     	// downtown crossing
-	codes["Downtown Crossing Station"] = ["RDTCN", "RDTCS"];
+	markers.push(new google.maps.Marker({title: "Downtown Crossing Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RDTCN", scode: "RDTCS"}));
 	redline_north.push(new google.maps.LatLng(42.352271,-71.055242));     	// south station
-	codes["South Station"] = ["RSOUN", "RSOUS"];
+	markers.push(new google.maps.Marker({title: "South Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RSOUN", scode: "RSOUS"}));
 	redline_north.push(new google.maps.LatLng(42.342622,-71.056967));		// broadway
-	codes["Broadway Station"] = ["RBRON", "RBROS"];
+	markers.push(new google.maps.Marker({title: "Broadway Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RBRON", scode: "RBROS"}));
 	redline_north.push(new google.maps.LatLng(42.330154,-71.057655));     	// andrew
-	codes["Andrew Station"] = ["RANDN", "RANDS"];
+	markers.push(new google.maps.Marker({title: "Andrew Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RANDN", scode: "RANDS"}));
 	intersect = new google.maps.LatLng(42.320685,-71.052391);				// jfk/umass
 	redline_north.push(intersect);     	
 	redline_braintree.push(intersect);
 	redline_ashmont.push(intersect);								
-	codes["JFK/UMass Station"] = ["RJFKN", "RJFKS"];
+	markers.push(new google.maps.Marker({title: "JFK/UMass Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RJFKN", scode: "RJFKS"}));
+	 
+	i = 1;
 	redline_ashmont.push(new google.maps.LatLng(42.31129,-71.053331));		// savin hill
-	codes["Savin Hill Station"] = ["RSAVN", "RSAVS"];
+	markers.push(new google.maps.Marker({title: "Savin Hill Station", position: redline_ashmont[i++], icon: t_icon, map: map, ncode: "RSAVN", scode: "RSAVS"}));
 	redline_ashmont.push(new google.maps.LatLng(42.300093,-71.061667));     // fields corner
-	codes["Fields Corner Station"] = ["RFIEN", "RFIES"];
+	markers.push(new google.maps.Marker({title: "Fields Corner Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RFIEN", scode: "RFIES"}));
 	redline_ashmont.push(new google.maps.LatLng(42.29312583,-71.06573796)); // shawmut
-	codes["Shawmut Station"] = ["RSHAN", "RSHAS"];
+	markers.push(new google.maps.Marker({title: "Shawmut Station", position: redline_ashmont[i++], icon: t_icon, map: map, ncode: "RSHAN", scode: "RSHAS"}));
 	redline_ashmont.push(new google.maps.LatLng(42.284652,-71.064489));		// ashmont
-	codes["Ashmont Station"] = [null, "RASHS"];
+	markers.push(new google.maps.Marker({title: "Ashmont Station", position: redline_ashmont[i++], icon: t_icon, map: map, ncode: null, scode: "RASHS"}));
+	
+	i = 1;
 	redline_braintree.push(new google.maps.LatLng(42.275275,-71.029583));	// north quincy
-	codes["North Quincy Station"] = ["RNQUN", "RNQUS"];
+	markers.push(new google.maps.Marker({title: "North Quincy Station", position: redline_braintree[i++], icon: t_icon, map: map, ncode: "RNQUN", scode: "RNQUS"}));
 	redline_braintree.push(new google.maps.LatLng(42.2665139,-71.0203369)); // wollaston
-	codes["Wollaston Station"] = ["RWOLN", "RWOLS"];
+	markers.push(new google.maps.Marker({title: "Wollaston Station", position: redline_braintree[i++], icon: t_icon, map: map, ncode: "RWOLN", scode: "RWOLS"}));
 	redline_braintree.push(new google.maps.LatLng(42.251809,-71.005409));	// quincy center
-	codes["Quincy Center Station"] = ["RQUCN", "RQUCS"];
+	markers.push(new google.maps.Marker({title: "Quincy Center Station", position: redline_north[i++], icon: t_icon, map: map, ncode: "RQUCN", scode: "RQUCS"}));
 	redline_braintree.push(new google.maps.LatLng(42.233391,-71.007153));	// quincy adams
-	codes["Quincy Adams Station"] = ["RQUAN", "RQUAS"];
+	markers.push(new google.maps.Marker({title: "Quincy Adams Station", position: redline_braintree[i++], icon: t_icon, map: map, ncode: "RQUAN", scode: "RQUAS"}));
 	redline_braintree.push(new google.maps.LatLng(42.2078543,-71.0011385)); // braintree
-	codes["Braintree Station"] = [null, "RBRAS"];
-	for(var i in redline_north) {
-		markers.push(new google.maps.Marker({title: north_names[i], position: redline_north[i], icon: t_icon, map: map}));
-	}
-	for(i = 1; i < redline_ashmont.length; i++) {
-		markers.push(new google.maps.Marker({title: ashmont_names[i], position: redline_ashmont[i],  icon: t_icon, map: map}));
-	}
-	for(i = 1; i < redline_braintree.length; i++) {
-		markers.push(new google.maps.Marker({title: braintree_names[i], position: redline_braintree[i], icon: t_icon, map: map}));
-	}
+	markers.push(new google.maps.Marker({title: "Braintree Station", position: redline_braintree[i++], icon: t_icon, map: map, ncode: null, scode: "RBRAS"}));
 	addClickListeners();
 }
+
 
 function addClickListeners() {
 	for(var i in markers) {
 		google.maps.event.addListener(markers[i], 'click', function(){
 			request = new XMLHttpRequest();
-			request.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json");
+			request.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json", false);
 			request.send(null);
+			
 			request.onreadystatechange = function() {
 				if(request.readyState == request.DONE) {
 					if(request.status == 200){
 						json_response = JSON.parse(request.responseText);
-						name = this.title;
-						northcode = codes[name][0];
-						southcode = codes[name][1];
-						content = "<h3>" + name + "</h3> <table><tr><th>Direction</th><th>Time Until Arrival</th></tr>";
-						for(var j in json_response) {
-							key = json_response[j].PlatformKey;
-							if(this.InformationType == "Predicted") {
-								if(key == northcode) {
-									content += "<tr><td>Northbound</td><td>" + this.TimeRemaining + '</td></tr>';
-								} else if(key == southcode) {
-									content += "<tr><td>Southbound</td><td>" + this.TimeRemaining + '</td></tr>';
-								}
-							}
-						}				
-						content += "</table>";
-						infowindow = new google.maps.InfoWindow({
-							map: map,
-							position: this.position, 
-							content: content
-						});
+						
 					} 
 					else {
 						printMessage("Error: " + request.status + " when retreiving JSON listing.");
 					}
 				} 
 			}
+			/* This is the only solution we found -- adding another get request to wait for 
+			 * previous request to finish. Credit to Trevor Rothouse. 
+			 */
+			request.open("GET", "http://mbtamap-cedar.herokuapp.com/mapper/redline.json", false);
+			request.send(null);
+			request.onreadystatechange = function() {}
+			
+			
+			printSchedule(this);
+			function printSchedule(marker) {	
+				name = marker.title;
+				northcode = marker.ncode;
+				southcode = marker.scode;
+				content = "<h3>" + name + "</h3> <table><tr><th>Direction</th><th>Time Until Arrival</th></tr>";
+				for(var j in json_response) {
+					key = json_response[j].PlatformKey;
+					if(json_response[j].InformationType == "Predicted") {
+						if(key == northcode) {
+							content += "<tr><td>Northbound</td><td>" + json_response[j].TimeRemaining + '</td></tr>';
+						} else if(key == southcode) {
+							content += "<tr><td>Southbound</td><td>" + json_response[j].TimeRemaining + '</td></tr>';
+						}
+					}
+				}				
+				content += "</table>";
+				if(open_infowindow != null) open_infowindow.close();
+				open_infowindow = new google.maps.InfoWindow({
+					map: map,
+					position: marker.position, 
+					content: content
+				});
+			}
+			
 		});
 	}
 }
@@ -156,12 +161,14 @@ function findWaldoandCarmen(mypos) {
 						var msg = json_response[i].loc.note;
 						var pos = new google.maps.LatLng(lat, lng);
 						new google.maps.Marker({title: name, position: pos, icon: icon, map: map});
-						var dist = calculateDistance(mypos, pos);
-						printMessage(name + " is " + dist.toFixed(2) + " miles away. Note: " + msg);
+						if(mypos) {
+							var dist = calculateDistance(mypos, pos);
+							printMessage(name + " is " + dist.toFixed(2) + " miles away. Note: " + msg);
+						}
 					}
 				} 
 				else {
-					printMessage("Error: " + request.status + " when retreiving JSON listing.");
+					printMessage("Error: (" + request.status + ") when retreiving JSON listing.");
 				}
 			} 
 		}
@@ -177,14 +184,11 @@ function findMyLocation() {
 			function(position) {
 				mypos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 				station = findClosestMarker(mypos);
-				infowindow = new google.maps.InfoWindow({
+				if(open_infowindow != null) open_infowindow.close();
+				open_infowindow = new google.maps.InfoWindow({
 					map: map,
 					position: mypos, 
 					content: "<h3> Found you! </h3> Closest station is <strong>" + station.title + "</strong> at a distance of " + station.distance.toFixed(2) + " miles."
-				});
-				positionmarker = new google.maps.Marker({
-					map: map,
-					position: mypos
 				});
 				mypos_to_station = new google.maps.Polyline({
 					map: map,
@@ -193,16 +197,22 @@ function findMyLocation() {
 					strokeOpacity: 0.5,
 					strokeWeight: 8
 				});
+				positionmarker = new google.maps.Marker({
+					map: map,
+					position: mypos
+				});
 				map.setCenter(mypos);
 				findWaldoandCarmen(mypos);
 			}, 
 			function() {
 				printMessage("Error: cannot get geolocation. You may have it turned off.");
+				findWaldoandCarmen();
 			}
 		);
 	}
 	else {  // Browser doesn't support Geolocation
 		printMessage("Error: your browser has no support for geolocation.");
+		findWaldoandCarmen();
 	}
 }
 
