c1882fa177a560791c6be9c313822dfa62b8ca4b
Louis Rassaby <louis.rassaby@gmail.com>
2016-05-10 23:31:11 -0400

    Typed name

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$(function () {
+    $(".typed-header").typed({
+        strings: ["<span style='color:#004710'>~/</span>louis-rassaby<span style='color:#004710'>/</span>"],
+        typeSpeed: 50
+    });
+});
