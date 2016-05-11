4107aac19038a86ef8be5d555a465628f7f13263
Louis Rassaby <louis.rassaby@gmail.com>
2016-05-11 11:04:24 -0400

    update typing start delay

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,6 @@ $(function () {
     $(".typed-header").typed({
         strings: ["<span style='color:#006e20'>~/^200</span>louis^200-^100rassaby^100<span style='color:#006e20'>/</span>"],
         typeSpeed: 80,
-        startDelay: 800
+        startDelay: 1000
     });
 });
