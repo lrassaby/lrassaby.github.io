84053a7c1c3223e40018fcfef977194262a4d71c
Louis Rassaby <louis.rassaby@gmail.com>
2016-05-10 23:33:46 -0400

    Changed color and speed slightly

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed-header").typed({
-        strings: ["<span style='color:#004710'>~/</span>louis-rassaby<span style='color:#004710'>/</span>"],
-        typeSpeed: 50
+        strings: ["<span style='color:#006e20'>~/</span>louis-rassaby<span style='color:#006e20'>/</span>"],
+        typeSpeed: 60
     });
 });
