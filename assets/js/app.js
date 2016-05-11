f29a8a8f2b68ba3235da0f4c62ea2e2263b4a9cc
Louis Rassaby <louis.rassaby@gmail.com>
2016-05-11 09:43:41 -0400

    Style updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(function () {
     $(".typed-header").typed({
         strings: ["<span style='color:#006e20'>~/</span>louis-rassaby<span style='color:#006e20'>/</span>"],
-        typeSpeed: 60
+        typeSpeed: 80,
+        startDelay: 800
     });
 });
