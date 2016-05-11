51ce04977b552816b67bc2876b8e3f9cdeb3f8c7
Louis Rassaby <louis.rassaby@gmail.com>
2016-05-11 11:02:53 -0400

    Fix more styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".typed-header").typed({
-        strings: ["<span style='color:#006e20'>~/</span>louis-rassaby<span style='color:#006e20'>/</span>"],
+        strings: ["<span style='color:#006e20'>~/^200</span>louis^200-^100rassaby^100<span style='color:#006e20'>/</span>"],
         typeSpeed: 80,
         startDelay: 800
     });
