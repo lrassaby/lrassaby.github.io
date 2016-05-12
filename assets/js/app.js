1ffc0664f4d6bd3100a625bd38890a7e7ade0151
Louis Rassaby <louis.rassaby@gmail.com>
2016-05-11 20:03:53 -0400

    Made UI more beautiful

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,16 @@
-$(function () {
+$(document).ready(function () {
     $(".typed-header").typed({
         strings: ["<span style='color:#006e20'>~/^200</span>louis^200-^100rassaby^100<span style='color:#006e20'>/</span>"],
         typeSpeed: 80,
         startDelay: 1000
     });
-});
-
-$(document).ready(function () {
     $(".navbar-nav li a").click(function (event) {
         $(".navbar-collapse").collapse('hide');
+    });
+    $('#fullpage').fullpage({
+        anchors: ['home', 'about', 'about2'],
+        menu: '#main-menu',
+        fixedElements: '#navbar-main',
+        sectionsColor: ['white', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
     });
 });
