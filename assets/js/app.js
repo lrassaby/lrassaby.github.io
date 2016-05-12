fe784c5dafb82684888e5094136a614ce19a47b5
Louis Rassaby <louis.rassaby@gmail.com>
2016-05-11 23:55:14 -0400

    Fix color scheme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
     $(".typed-header").typed({
-        strings: ["<span style='color:#006e20'>~/^200</span>louis^200-^100rassaby^100<span style='color:#006e20'>/</span>"],
+        strings: ["<span class='green'>~/^200</span>louis^200-^100rassaby^100<span class='green'>/</span>"],
         typeSpeed: 80,
         startDelay: 1000
     });
+
     $(".navbar-nav li a").click(function (event) {
         $(".navbar-collapse").collapse('hide');
     });
+    
     $('#fullpage').fullpage({
         anchors: ['home', 'about', 'about2'],
         menu: '#main-menu',
